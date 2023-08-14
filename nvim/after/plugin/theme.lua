403a5ab42ac832c914e5b3bb8b41ac5f460fe537
hito-kotaro <tohi.work@gmail.com>
2023-08-14 22:48:54 +0900

    nvim add config

---
+++ nvim/after/plugin/theme.lua
@@ -0,0 +1,9 @@
+vim.cmd([[colorscheme PaperColor]])
+
+-- background setting
+vim.cmd('highlight Normal guibg=NONE ctermbg=NONE')
+vim.cmd('highlight NonText guibg=NONE ctermbg=NONE')
+vim.cmd('highlight LineNr guibg=NONE ctermbg=NONE')
+vim.cmd('highlight Folded guibg=NONE ctermbg=NONE')
+vim.cmd('highlight EndOfBuffer guibg=NONE ctermbg=NONE')
+
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,5 @@
+require("hito.base")
+require("hito.plugins")
+require("hito.highlight")
+require("hito.maps")
 
